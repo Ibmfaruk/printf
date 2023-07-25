a0e70bc76a3cc621a552e7fd99cfa7ae3b421432
Don-Snave <132457434+Don-Snave@users.noreply.github.com>
2023-07-25 21:20:27 +0100

    Update functions1.c

--- functions1.c
+++ functions1.c
@@ -126,6 +126,7 @@ int print_hexa_upper(va_list types, char buffer[],
  * @size: Size specifier
  * @size: Size specification
  * Return: Number of chars printed
+ * Comments added Partner edit
  */
 int print_hexa(va_list types, char map_to[], char buffer[],
 	int flags, char flag_ch, int width, int precision, int size)
